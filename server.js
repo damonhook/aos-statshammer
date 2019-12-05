ec9f7077bcfce6ccce62f56e0f3582367fe340fb
Damon Hook <damon.hook@takealot.com>
2019-12-05 10:41:34 +0200

    Add production build config for heroku deploy

--- server.js
+++ server.js
@@ -19,4 +19,14 @@ app.post('/api/compare', (req, res) => {
   res.send(compareUnits(req.body));
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
