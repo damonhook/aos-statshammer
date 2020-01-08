77bf5267aecfcbc2c7f257c7130ac9bbba403ccd
Damon Hook <damon.hook@takealot.com>
2020-01-08 14:38:20 +0200

    About logo is clickable

--- client/src/containers/About/About.tsx
+++ client/src/containers/About/About.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from 'components/AppBar';
-import { EPages } from 'types/routes';
+import { EPages, getRoute } from 'types/routes';
 import { useReadFromFile } from 'hooks';
 import ReactMarkdown from 'react-markdown';
-import { Paper, Theme, Typography, Divider, Button, CircularProgress } from '@material-ui/core';
+import { Paper, Theme, Typography, Divider, Button, CircularProgress, IconButton } from '@material-ui/core';
 import { GitHub, Reddit, Assessment as Logo } from '@material-ui/icons';
 import Footer from 'components/Footer';
 import BottomNavigation from 'components/BottomNavigation';
 import { grey } from '@material-ui/core/colors';
 import { scrollToRef } from 'utils/scrollIntoView';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) => ({
   about: {
@@ -23,19 +24,27 @@ const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     maxWidth: '900px',
     textAlign: 'center',
-    display: 'flex',
     flexDirection: 'column',
+    display: 'flex',
   },
   paper: {
     padding: theme.spacing(3),
     margin: theme.spacing(2),
+    flexDirection: 'column',
+    display: 'flex',
+    flex: 1,
   },
   loader: {
     margin: theme.spacing(4),
   },
   logo: {
-    fontSize: '128px',
     margin: '0 auto',
+  },
+  logoIcon: {
+    fontSize: '7rem',
+  },
+  md: {
+    marginTop: -theme.spacing(2),
   },
   divider: {
     marginTop: theme.spacing(3),
@@ -60,24 +69,31 @@ const useStyles = makeStyles((theme: Theme) => ({
 const About = () => {
   const classes = useStyles();
   const content = useReadFromFile('about.md');
+  const history = useHistory();
   const ref = useRef(null);
 
   useEffect(() => {
     scrollToRef(ref, true);
   });
 
+  const handleLogoClick = () => {
+    history.push(getRoute(EPages.HOME));
+  };
+
   return (
     <div className={classes.about} ref={ref}>
       <AppBar variant={EPages.ABOUT} />
       <div className={classes.wrapper}>
         <Paper className={classes.paper}>
-          <Logo className={classes.logo} />
+          <IconButton onClick={handleLogoClick} className={classes.logo}>
+            <Logo color="primary" className={classes.logoIcon} />
+          </IconButton>
           {!content && (
             <div className={classes.loader}>
               <CircularProgress size="6rem" />
             </div>
           )}
-          <ReactMarkdown source={content} />
+          <ReactMarkdown source={content} className={classes.md} />
           <Divider className={classes.divider} />
           <Typography variant="h6">Social:</Typography>
           <div className={classes.socialActions}>
